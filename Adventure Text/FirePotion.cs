e523bedfef878ff9723df43d06ea3b73061421e8
AiLaicNothing <ailaicnothing@gmail.com>
2026-04-23 20:45:46 -0500

    Items

---
+++ Adventure Text/FirePotion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure_Text
+{
+    internal class FirePotion : Item
+    {
+        int damage = 15;
+
+        public FirePotion()
+        {
+            name = "Poción de fuego";
+            description = $"Inflige {damage} de daño";
+        }
+
+        protected override void Fuction(Entity target)
+        {
+            target.ReciveDamage(damage);
+        }
+    }
+}
---
+++ Adventure Text/HealPotion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Adventure_Text
+{
+    internal class HealPotion: Item
+    {
+        int heal = 15;
+
+        public HealPotion()
+        {
+            name = "Poción de cura";
+            description = $"Cura {heal} de vida";
+        }
+
+        protected override void Fuction(Entity target)
+        {
+            target.Heal(heal);
+        }
+    }
+}
